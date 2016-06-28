c48bc55c9d01babf8e7bab99237a70edca45c51f
MirChangBin <mac@macdeMacintosh.local>
2016-06-28 16:37:05 +0800

    顶部图片用afnetwotking请求

--- PinDuoDuoTest/PinDuoDuoTest/NetWorkRequestModel.m
+++ PinDuoDuoTest/PinDuoDuoTest/NetWorkRequestModel.m
@@ -14,7 +14,19 @@
 
 #pragma mark 第一个顶部滚动的图片进行网络请求  返回URL，，，好像也不需要了，工具生成模型就够了
 
-
+-(void)topScrollViewImage
+{
+    AFHTTPSessionManager*manager=[AFHTTPSessionManager manager];
+    [manager GET:@"http://apiv2.yangkeduo.com/subjects" parameters:nil success:^(NSURLSessionDataTask * _Nonnull task, id  _Nonnull responseObject) {
+        
+        
+        
+    } failure:^(NSURLSessionDataTask * _Nonnull task, NSError * _Nonnull error) {
+        
+    }];
+    
+    
+}
 
 
 
