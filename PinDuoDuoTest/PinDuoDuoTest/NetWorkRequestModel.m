0dee28c0d322f8ddd11c1efba6fe6426f3305708
MirChangBin <mac@macdeMacintosh.local>
2016-06-29 10:38:49 +0800

    添加模型数据输出，保证安全
    不知道为什么输出的时候不提示属性选择。。都要一个一个复制，，还因为没有属性或者错了

--- PinDuoDuoTest/PinDuoDuoTest/NetWorkRequestModel.m
+++ PinDuoDuoTest/PinDuoDuoTest/NetWorkRequestModel.m
@@ -71,6 +71,14 @@
 #pragma mark  模型，打算直接传出去，一会看看有没有数据，可以在控制器里边的viewwillappear里边处理试试，其实最好传数组过去，这里就把需要的都得到出来在存储到数组，传过去在取出来也可以
         PDDHomeData*modelData=[PDDHomeData modelObjectWithDictionary:responseObject];
         
+#pragma mark 先试试数组可以取出来完整的麼   也是够了。。打的时候不提示！！！！！！要慢慢复制过来！！
+        NSLog(@"超值大牌的:=%@--count:=%lu",modelData.homeSuperBrand.goodsList,(unsigned long)modelData.homeSuperBrand.goodsList.count);
+        
+        NSLog(@"查看更多等:=%@--count=%lu",modelData.homeRecommendSubjects,(unsigned long)modelData.homeRecommendSubjects.count);
+        
+        NSLog(@"最多的数据:%@--count:%lu",modelData.goodsList,(unsigned long)modelData.goodsList.count);
+        
+        
         if ([_delegate respondsToSelector:@selector(sucessToGetData:modelData:)]) {
             [_delegate sucessToGetData:self modelData:modelData];
         }
