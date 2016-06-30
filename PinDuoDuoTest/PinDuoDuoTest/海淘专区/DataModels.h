848a4fba7cf1a8efc88237608a8793e2ccdc41bd
MirChangBin <mac@macdeMacintosh.local>
2016-06-30 09:00:04 +0800

    先暂时删掉，先不要这个model先
    时间原因，晚点在写。封装得不够好，或者说数据源不一样，需要重写的，虽然看起来界面一样，，但是数据源给的方式不一样，还容易两个不同数据源间有属性冲
    突，，好好处理！

--- PinDuoDuoTest/PinDuoDuoTest/海淘专区/DataModels.h
+++
@@ -1,8 +0,0 @@
-//
-//  DataModels.h
-//
-//  Created by   on 16/6/30
-//  Copyright (c) 2016 __MyCompanyName__. All rights reserved.
-//
-
-#import "PDDGroup.h"#import "PDDCountryList.h"#import "PDDPromotionList.h"#import "PDDGoodsList.h"#import "PDDRankDataModel.h"
